5f9daacf35c7ae44b691831e3fb27ffc9d81807a
Noble Osinachi <nobleosinachi98@gmail.com>
2023-11-19 15:59:29 +0100

    Set up the client side

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+using Sub4Me.Models;
+
+namespace Sub4Me.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Sub4Me.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
